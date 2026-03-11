784b86d414a9044359a4d944a9d449a411087e9c
ujjwal kumar <ujjwalsharmasharma09@gmail.com>
2026-03-11 10:56:54 +0530

    Time: 0 ms (100%), Space: 8.9 MB (67.08%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for(char c : s) {
+            if(c == '(' || c == '{' || c == '[') {
+                st.push(c);
+            }
+            else {
+                if(st.empty()) return false;
+
+                if((c == ')' && st.top() == '(') ||
+                   (c == '}' && st.top() == '{') ||
+                   (c == ']' && st.top() == '[')) {
+                    st.pop();
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+
+        return st.empty();
+    }
+};
