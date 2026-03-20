e2a3b52c5bae8b2f63e204f463aa513eb35f2244
ujjwal kumar <ujjwalsharmasharma09@gmail.com>
2026-03-20 11:19:36 +0530

    Time: 0 ms (100%), Space: 7.7 MB (99.78%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        long long res = 1;
+        int N = m + n - 2;
+        int r = min(m - 1, n - 1);
+
+        for(int i = 1; i <= r; i++) {
+            res = res * (N - r + i) / i;
+        }
+
+        return (int)res;
+    }
+};
