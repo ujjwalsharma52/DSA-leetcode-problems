052fdd9411bf5f11eb6bac309b9f6e50a9074e6c
ujjwal kumar <ujjwalsharmasharma09@gmail.com>
2026-07-25 11:24:41 +0530

    Time: 1229 ms (29.25%), Space: 0B (100%) - LeetHub - Time: O(N log N), Space: O(N)

---
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -0,0 +1,20 @@
+# Write your MySQL query statement below
+SELECT
+    d.name AS Department,
+    e.name AS Employee,
+    e.salary AS Salary
+FROM (
+    SELECT *,
+           DENSE_RANK() OVER (
+               PARTITION BY departmentId
+               ORDER BY salary DESC
+           ) AS rnk
+    FROM Employee
+) e
+JOIN Department d
+ON e.departmentId = d.id
+WHERE e.rnk <= 3;
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
