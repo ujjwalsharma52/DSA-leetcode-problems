60f9f481a7d6f49fdbe14b4dd3ad604f9ab231ac
ujjwal kumar <ujjwalsharmasharma09@gmail.com>
2026-07-25 11:46:09 +0530

    Time: 528 ms (35.03%), Space: 0B (100%) - LeetHub - Time: O(N^2), Space: O(N)

---
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+SELECT w1.id
+FROM Weather w1
+JOIN Weather w2
+ON DATEDIFF(w1.recordDate, w2.recordDate) = 1
+WHERE w1.temperature > w2.temperature;
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
