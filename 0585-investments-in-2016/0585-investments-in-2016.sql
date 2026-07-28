d6271bf3a2635233daa566718df80cf800ff4b14
ujjwal kumar <ujjwalsharmasharma09@gmail.com>
2026-07-28 11:38:31 +0530

    Time: 616 ms (43.48%), Space: 0B (100%) - LeetHub - Time: O(n^2), Space: O(n)

---
+++ 0585-investments-in-2016/0585-investments-in-2016.sql
@@ -0,0 +1,20 @@
+# Write your MySQL query statement below
+SELECT
+    ROUND(SUM(tiv_2016), 2) AS tiv_2016
+FROM Insurance
+WHERE tiv_2015 IN (
+    SELECT tiv_2015
+    FROM Insurance
+    GROUP BY tiv_2015
+    HAVING COUNT(*) > 1
+)
+AND (lat, lon) IN (
+    SELECT lat, lon
+    FROM Insurance
+    GROUP BY lat, lon
+    HAVING COUNT(*) = 1
+);
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
