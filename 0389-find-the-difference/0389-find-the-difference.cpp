a4f9ca529792c50a378254959df3ea66a776794e
ujjwal kumar <ujjwalsharmasharma09@gmail.com>
2026-07-30 11:52:43 +0530

    Time: 0 ms (100%), Space: 9 MB (96.34%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        char ans = 0;
+
+        for (char c : s)
+            ans ^= c;
+
+        for (char c : t)
+            ans ^= c;
+
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
