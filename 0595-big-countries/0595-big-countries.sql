ed395c773666844dc5347737aa90b03fbaae97aa
ujjwal kumar <ujjwalsharmasharma09@gmail.com>
2026-07-31 18:13:04 +0530

    Time: 350 ms (16.35%), Space: 0B (100%) - LeetHub - Time: O(N), Space: O(N)

---
+++ 0595-big-countries/0595-big-countries.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+SELECT
+    name,
+    population,
+    area
+FROM World
+WHERE area >= 3000000
+   OR population >= 25000000;
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
