cab84793fa7804439e4e35b4a4fae6277c4114b9
ujjwal kumar <ujjwalsharmasharma09@gmail.com>
2026-09-03 09:52:38 +0530

    Force committed solution - Time: O(1), Space: O(1)

---
+++ 12103-input-and-output-Hackerrank/12103-input-and-output.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+
+    cin >> a >> b >> c;
+
+    cout << a + b + c << endl;
+
+    return 0;
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
