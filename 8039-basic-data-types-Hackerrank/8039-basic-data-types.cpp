0b65fc2a492a999e4d94c407f18a439fce68c67d
ujjwal kumar <ujjwalsharmasharma09@gmail.com>
2026-09-03 09:57:16 +0530

    Force committed solution - Time: O(1), Space: O(1)

---
+++ 8039-basic-data-types-Hackerrank/8039-basic-data-types.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    int a;
+    long long b;
+    char c;
+    float d;
+    double e;
+
+    cin >> a >> b >> c >> d >> e;
+
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+    cout <<   d << endl;
+    cout << e << endl;
+
+    return 0;
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
